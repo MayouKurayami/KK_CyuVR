45daf2ff13b89062e58a8c85832b91fb44dfe1d7
MayouKurayami <mkw801021@gmail.com>
2020-01-10 15:22:48 -0800

    update AssemblyInfo

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,14 +1,15 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using static Bero.CyuVR.CyuLoaderVR;
 
-[assembly: AssemblyTitle("Kkt")]
+[assembly: AssemblyTitle("CyuVR")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Kkt")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("CyuVR")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("2fd732bc-5eda-4920-b023-36fa6e340d37")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion(Version)]
+[assembly: AssemblyVersion(Version)]
